cca36a7da7ccb05aa1fed4ede223facf79b75165
rajchampaneri <rajnileshbhai2004@gmail.com>
2024-06-24 15:31:09 +0530

    script.js

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-/*let String = "";
-let Buttons = document.querySelectorAll('.Button');
-Array.from(Buttons).forEach((Button)=>{
-    Button.addEventListener("click",(e)=>{
-        if(e.target.innerHTML == '='){
-            string=evel(string);
-            document.querySelector("input").value = string;
-        }
-        else if(e.target.innerHTML == "C"){
-            string="";
-            document.querySelector("input").value = string;
-        }
-        else{
-        console.log(e.target)
-        string = string + e.target.innerHTML;
-        document.querySelector('input').value = string;
-        }
-    })
-})*/
 function pick(val)
 {
     document.getElementById("t").value += val;
